c78f20d5424a81f44a8b4b466d5f7477c784e2b0
erupb <74407228+Erupb@users.noreply.github.com>
2022-03-27 13:38:56 +0300

    Tasks 12-13 are done! (Task 13 is inside this folder)

---
+++ src/t12/src/main/java/com/example/t12/T12Application.java
@@ -0,0 +1,14 @@
+package com.example.t12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class T12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T12Application.class, args);
+    }
+
+}
---
+++ src/t12/src/test/java/com/example/t12/T12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
